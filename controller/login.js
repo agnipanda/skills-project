8e0688862b214da2156526569576badf3ee7020c
agnipanda <agnipanda69@gmail.com>
2018-04-26 01:38:31 +0530

    idk

--- controller/login.js
+++ controller/login.js
@@ -8,23 +8,28 @@ var logUser=function(req,res){
 		roomid:req.body.logid,
 		password:req.body.logpass
 	});
-   	// var pw = logmodel.password;
+ 	// var pw = logmodel.password;
 	// var saltRounds = 10;
 	// var hash = bcrypt.hashSync(pw, saltRounds);
 	// logmodel.password = hash;
-  	signModel.findOne({roomid:logmodel.roomid,password:logmodel.password},function(err,user){
-  		if(err){
-  			console.log(err);
-  			return res.status(500).send();
-  		}
-  		if(!user){
-  			return res.render('logsign',{msg:'Invalid credentials..!!'})
-  		}
-  		req.session.user=user;
-  		return res.render('home');
-
-  	});
-  };
+  	signModel.findOne({roomid:logmodel.roomid},function(err,user){
+  		console.log(user);
+  		if(err) throw err;
+	      if(user){
+	        //bcrypt.compare(req.body.password,user.password,function(err,isMatch) {
+	          if(err) throw err;
+	          if(user.password==logmodel.password){
+	            var details = {
+	              "name" : user.name,
+	              "email" : user.email,
+	              "phone" : user.phone,
+	              "roomid" : user.roomid
+	            };
+				req.session.user = details;
+  			  }
+		  }
+	});
+};
 	 /*signModel.findOne({"roomid":logmodel.roomid},"password",function(err,response){
 	// 	var pw = logmodel.password;
 	// 	var bool = bcrypt.compareSync(pw, res);
